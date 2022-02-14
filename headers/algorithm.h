22ef839e8df1e0ef9ec3cb4eed02467ee6d66aeb
FWXG <krel2017@gmail.com>
2022-02-14 16:42:08 +0300

    algorithm

--- headers/algorithm.h
+++ headers/algorithm.h
@@ -1,44 +1,10 @@
 #pragma once
-#include <cstdint>
-#include <iostream>
+
 
 namespace alt
 {
-	template<typename T>
-	class vector
-	{
-	private:
-	    std::size_t m_max_size = 0;
-		std::size_t m_index = 0;
-		T* m_pointer = nullptr;
-		T* m_tmp_arr = nullptr;
-
-	public:
-
-		vector() = default;
-		~vector();
-
-		bool is_empty() const;
-
-		void add_elem(T elem);
-		void reserve(std::size_t num);
-		void resize(std::size_t num);
-		void insert(std::size_t pos, T elem);
-		void erase(std::size_t pos, T elem);
-		void del_last();
-		void clear();
-
-		std::size_t length() const;
-
-		T& operator[](const int index) const
-		{
-			return m_pointer[index];
-		}
 
 
-
-	};
-
-	#include "..\src\vector.tpp"
+	#include "..\src\algorithm.tpp"
 
 }
